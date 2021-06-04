892488cbf5553660f535bc55e26039b8dbfb3898
Arvind <heyarviind@gmail.com>
2021-06-04 11:40:19 +0530

    make rating component working

--- src/components/Rating.vue
+++ src/components/Rating.vue
@@ -1,9 +1,50 @@
 <template>
   <span>
-    <img src="../assets/star.svg" />
-    <img src="../assets/star.svg" />
-    <img src="../assets/star.svg" />
-    <img src="../assets/star.svg" />
-    <img src="../assets/star.svg" />
+    <Star
+      :state="getState(index)"
+      v-for="(x, index) in maxRating"
+      :key="`star-${x}`"
+    />
   </span>
 </template>
+
+<script>
+import Star from "./Star";
+
+export default {
+  props: {
+    value: {
+      default: 0,
+    },
+  },
+  components: {
+    Star,
+  },
+  data: () => ({
+    maxRating: 5,
+    arr: [],
+  }),
+  created() {
+    if (this.value >= 0 && this.value <= this.maxRating) {
+      const decimalValue = this.value - Math.floor(this.value);
+      let normalValue = Math.floor(this.value);
+
+      for (let i = 1; i <= normalValue; i++) {
+        this.arr.push(1);
+        if (i == normalValue && decimalValue) {
+          this.arr.push(decimalValue);
+        }
+      }
+    }
+  },
+  methods: {
+    getState(index) {
+      if (this.arr[index] == 1) return "filled";
+      if (this.arr[index] > 0 && this.arr[index] < 1) return "half";
+      if (!this.arr[index]) return "empty";
+
+      return "empty";
+    },
+  },
+};
+</script>
---
+++ src/components/Star.vue
@@ -0,0 +1,18 @@
+<template>
+  <span>
+    <img v-if="state == 'filled'" src="../assets/star-filled.svg" />
+    <img v-if="state == 'half'" src="../assets/star-half.svg" />
+    <img v-if="state == 'empty'" src="../assets/star-empty.svg" />
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    state: {
+      type: String,
+      default: "empty",
+    },
+  },
+};
+</script>
