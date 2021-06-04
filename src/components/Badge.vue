7b78454fb35fb7a4fbb2443f5da5e7419357a8ba
Arvind <heyarviind@gmail.com>
2021-06-04 12:51:29 +0530

    change Button to Badge

---
+++ src/components/Badge.vue
@@ -0,0 +1,22 @@
+<template>
+  <span class="badge">
+    <slot />
+  </span>
+</template>
+
+<style scoped>
+.badge {
+  padding: 4px 8px;
+  border-radius: 50px;
+  display: inline-flex;
+  border: none;
+  color: #fff;
+  align-items: center;
+  font-size: 12px;
+  background-color: rgba(255, 255, 255, 0.4);
+}
+
+.badge img {
+  margin-right: 4px;
+}
+</style>
