7eda07151b297f525d7cd90b5136416b55652d63
Arvind <heyarviind@gmail.com>
2021-06-04 13:30:11 +0530

    update rating component to respect prop change in storybook

--- src/components/Rating.vue
+++ src/components/Rating.vue
@@ -14,7 +14,7 @@ import Star from "./Star";
 export default {
   props: {
     value: {
-      default: 0,
+      default: 3.5,
     },
   },
   components: {
@@ -25,25 +25,39 @@ export default {
     arr: [],
   }),
   created() {
-    if (this.value >= 0 && this.value <= this.maxRating) {
-      const decimalValue = this.value - Math.floor(this.value);
-      let normalValue = Math.floor(this.value);
-
-      for (let i = 1; i <= normalValue; i++) {
-        this.arr.push(1);
-        if (i == normalValue && decimalValue) {
-          this.arr.push(decimalValue);
-        }
-      }
-    }
+    this.hydrateRating();
   },
   methods: {
+    hydrateRating() {
+      if (this.value >= 0 && this.value <= this.maxRating) {
+        const decimalValue = this.value - Math.floor(this.value);
+        let normalValue = Math.floor(this.value);
+
+        // make the arr empty if the value changes in storybook
+        this.arr = [];
+
+        for (let i = 1; i <= normalValue; i++) {
+          this.arr.push(1);
+          if (i == normalValue && decimalValue) {
+            this.arr.push(decimalValue);
+          }
+        }
+      }
+    },
     getState(index) {
       if (this.arr[index] == 1) return "filled";
       if (this.arr[index] > 0 && this.arr[index] < 1) return "half";
       if (!this.arr[index]) return "empty";
 
       return "empty";
+    },
+  },
+  // need to watch the rating value, as this can be changed in storybook
+  // this rating component is dependent on arr variable so it has to be
+  // updated on every prop change
+  watch: {
+    value: function() {
+      this.hydrateRating();
     },
   },
 };
